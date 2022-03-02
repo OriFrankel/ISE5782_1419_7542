5c2ef242803f5e50fe5b9b29048ca1f276660aab
ofrankel <ofrankel@L320-52-225.acad.jct.ac.il>
2022-03-02 17:51:52 +0200

    geometries

--- src/geometries/Tube.java
+++ src/geometries/Tube.java
@@ -2,13 +2,26 @@ package geometries;
 
 import primitives.Point;
 import primitives.Vector;
-
+import primitives.Ray;
 public class Tube implements Geometry {
-
+	Ray axisRay;
+	double radius;
+	public Tube(Ray ray, double d) {
+		axisRay = ray;
+		radius = d;
+	}
 	@Override
 	public Vector getNormal(Point p) {
-		// TODO Auto-generated method stub
 		return null;
 	}
-
+	public Ray getAxisRay() {
+		return axisRay;
+	}
+	public double getRadius() {
+		return radius;
+	}
+	@Override
+	public String toString() {
+		return axisRay.toString() + " and radius: " + ((Double)radius).toString();
+	}
 }
