adf2944ca33bb5409141a831bfb8a2390c5b1f4a
ofrankel <ofrankel@S227-53-87.acad.jct.ac.il>
2022-03-12 20:32:12 +0200

    bug fix

--- src/unittest/PlaneTests.java
+++ src/unittest/PlaneTests.java
@@ -14,7 +14,7 @@ class PlaneTests {
 	void testGetNormal() {
 		Point p1=new Point(1,2,3);
 		Point p2=new Point(4,5,6);
-		Point p3=new Point(7,8,9);
+		Point p3=new Point(37,52,11);
 		Plane p=new Plane(p1,p2,p3);
 		// ============ Equivalence Partitions Tests ==============
 		//some point
