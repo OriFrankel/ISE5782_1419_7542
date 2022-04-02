18c645097fcd87b3e4bc81e4a84185519a7bf647
euchr <euchr@Frankelim>
2022-04-03 01:19:47 +0300

    ex05

--- src/lighting/AmbientLight.java
+++ src/lighting/AmbientLight.java
@@ -2,29 +2,37 @@
  * 
  */
 package lighting;
+
 import primitives.*;
+
 /**
  * class for constant light
+ * 
  * @author ori frankel and yair sprecher
  */
 public class AmbientLight {
 	final private Color intensity;
+
 	/**
 	 * constructor for AmbientLight, gets color and Discount factor
+	 * 
 	 * @param color the color
-	 * @param k Discount factor
+	 * @param k     Discount factor
 	 */
-	public AmbientLight(Color color,Double3 k) {
-		intensity=color.reduce(k);
+	public AmbientLight(Color color, Double3 k) {
+		intensity = color.reduce(k);
 	}
+
 	/**
 	 * constructor, initializes intensity to black
 	 */
 	public AmbientLight() {
-		intensity=Color.BLACK;
+		intensity = Color.BLACK;
 	}
+
 	/**
 	 * get the intensity of the light
+	 * 
 	 * @return the intensity
 	 */
 	public Color getIntensity() {
