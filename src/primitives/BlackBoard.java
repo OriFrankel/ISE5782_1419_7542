f981cc7efbc8331e94aca55df6c17ab250d33d7d
ofrankel <ofrankel@B305-52-181.acad.jct.ac.il>
2022-05-22 14:44:20 +0300

    blackboard

--- src/primitives/BlackBoard.java
+++ src/primitives/BlackBoard.java
@@ -1,11 +1,8 @@
-package renderer;
+package primitives;
 
 
 import java.util.LinkedList;
 import java.util.List;
-
-import primitives.Point;
-import primitives.Vector;
 
 /**
  * Class for black board
@@ -14,25 +11,28 @@ import primitives.Vector;
  *
  */
 public class BlackBoard {
-	private int amountOfRays = 1;
+	private int squareSize = 1;
+	private Point center;
 	private Vector vUp;
 	private Vector vRight;
-	private double space;
+	private double space=1;
 	private static Vector firstTry = new Vector(0, 0, 1);
 	private static Vector secondTry = new Vector(0, 1, 0);
+	private List<Point> list=null;
 
 	/**
 	 * constructor for BlackBoard
 	 * 
 	 * @param v vector to the board
 	 */
-	public BlackBoard(Vector v) {
+	public BlackBoard(Vector v,Point center) {
 		try {
 			vUp = v.crossProduct(firstTry).normalize();
 		} catch (IllegalArgumentException e) {
 			vUp = v.crossProduct(secondTry).normalize();
 		}
 		vRight = vUp.crossProduct(v).normalize();
+		this.center=center;
 	}
 
 	/**
@@ -42,7 +42,8 @@ public class BlackBoard {
 	 * @return the object itself
 	 */
 	public BlackBoard setAmountOfRays(int amountOfRays) {
-		this.amountOfRays = (int)Math.sqrt(amountOfRays)/2;
+		this.squareSize = (int)Math.sqrt(amountOfRays)/2;
+		if(this.squareSize<1)this.squareSize =1;
 		return this;
 	}
 
@@ -58,11 +59,28 @@ public class BlackBoard {
 	}
 
 	/**
-	 * 
-	 * @return
+	 * calculate set of points
+	 * @return the object
 	 */
-	public List<Point> calculatePoints() {
-		List<Point> list = new LinkedList<Point>();
-		return null;
+	public BlackBoard calculatePoints() {
+		list = new LinkedList<Point>();
+		Vector v1=vUp.scale(space),v2=vRight.scale(space);
+		for(int i=-squareSize;squareSize>=i;++i) {
+			Point cur=center.subtract(v1.scale(squareSize));
+			if(i!=0)
+				cur=cur.add(v2.scale(i));
+			for(int j=-squareSize;squareSize>=j;++j) {
+				list.add(cur);
+				cur=cur.add(v1);
+			}
+		}
+		return this;
+	}
+	/**
+	 * get the list of points
+	 * @return the list
+	 */
+	public List<Point> getList(){
+		return list;
 	}
 }
